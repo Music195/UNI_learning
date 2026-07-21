1eea4ba5bc4dbe7e53e2e5d0c0524a37c15c9b61
Music195 <minthetkhine195@gmail.com>
2026-07-21 16:43:32 +0900

    Tracking changes in IntMinMax10.java

--- share/csprog/intro/12/IntMinMax10.java
+++ share/csprog/intro/12/IntMinMax10.java
@@ -5,14 +5,14 @@ class IntMinMax10 {
         Random rd = new Random();
         int[] b = new int[10];
 
-        int min = b[0];
-        int max = b[0];
-
         for (int i = 0; i < b.length; i++) {
              b[i] = rd.nextInt(100);
              System.out.print(b[i] + " ");
         }
         System.out.println();
+
+        int min = b[0];
+        int max = b[0];
 
         for (int v: b) {
              if (max <= v) {
