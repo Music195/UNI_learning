53952ec8482cd3bbaabaedb272d0cd15d024e18d
Music195 <minthetkhine195@gmail.com>
2026-07-21 16:16:17 +0900

    Tracking the change in 12 folder

--- share/csprog/intro/12/FizzBuzzA.java
+++ share/csprog/intro/12/FizzBuzzA.java
@@ -1,30 +1,18 @@
 class FizzBuzzA {
-static
-public
-void
-main(
-String[]
-    argv) { for
-                                        (int
-i=1;
-         i<100; i++
-){ if (i %
-3 != 0
-          && i
-% 5 != 0
-) { System.
-out.print(i);
-                         }
-                   if (i % 3
-== 0) { System.out.print
-("Fizz");
-}  if (i % 5 ==
-0) { System.
-out.print(
-    "Buzz"); }
-        System.
-out.
-print
-(" "
-);    }    System.out.println(); }
-                                                 }
+    static public void main(
+            String[] argv) {
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                System.out.print(i);
+            }
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+            }
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+}
--- share/csprog/intro/12/IntMinMax10.java
+++ share/csprog/intro/12/IntMinMax10.java
@@ -1,13 +1,29 @@
 import java.util.Random;
 
-Class IntMinMax10 {
+class IntMinMax10 {
     static public void main (String args[]) {
+        Random rd = new Random();
         int[] b = new int[10];
 
-        int min, max;
+        int min = b[0];
+        int max = b[0];
 
-        for 
+        for (int i = 0; i < b.length; i++) {
+             b[i] = rd.nextInt(100);
+             System.out.print(b[i] + " ");
+        }
+        System.out.println();
 
-        for (int v: b) {}
+        for (int v: b) {
+             if (max <= v) {
+                  max = v;
+             }
+             if (min >= v) {
+                  min = v;
+             }
+
+        }
+        System.out.print("Max: " + max + "\n" + "Min: " + min);
+        System.out.println();
     }
 }
