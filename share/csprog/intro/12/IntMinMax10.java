30e0e85be672987e862a06643cb12baf0dd7526f
Music195 <minthetkhine195@gmail.com>
2026-07-21 12:17:25 +0900

    adding praticed file in 12

---
+++ share/csprog/intro/12/IntMinMax10.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+Class IntMinMax10 {
+    static public void main (String args[]) {
+        int[] b = new int[10];
+
+        int min, max;
+
+        for 
+
+        for (int v: b) {}
+    }
+}
